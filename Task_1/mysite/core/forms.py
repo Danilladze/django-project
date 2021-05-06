7aa09f3226d0ae08f798fa0b8dbaa5f1b7cb86a8
daniladze <daniltimasehv@yandex.ru>
2021-05-06 13:26:16 -0700

    Update

--- Task_1/mysite/core/forms.py
+++ Task_1/mysite/core/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 from .models import link
-from django.core.exceptions import ValidationError
-from tldextract import extract
+from django.core.validators import URLValidator
 
     
 class LinkForm(forms.ModelForm):
     link = forms.CharField(max_length=2000, widget=forms.TextInput(attrs={"class":"form-control me-2","placeholder": "Input the link","aria-label":"Search","name":"link_holder"}))
+    
     class Meta:
         model = link
         fields = ['link']
@@ -13,8 +13,9 @@ class LinkForm(forms.ModelForm):
     def clean_link(self):
         domain_zone = ["ru","com","org","xyz","ua","рф"]
         Link = self.cleaned_data['link']
-        tsd,td,tsu = extract(Link)
-        if (tsu not in domain_zone):
-            raise ValidationError('Input correct link!')
-        else:
-            return(Link)
+        url_validator = URLValidator(Link)
+        try:
+            url_validator(Link)
+        except:
+            raise forms.ValidationError("Invalid Link!")
+        return(Link)
